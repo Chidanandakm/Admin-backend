c491c1d7c68688e4d1729b55ed9e1c0a12144f1c
chidananda <chidananda@iterator.in>
2022-05-07 11:39:12 +0530

    user

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,17 +17,17 @@ const getUsers = async (req, res) => {
 };
 
 const Register = async (req, res) => {
-    const { email, password } = req.body;
+    const { name, email, password } = req.body;
 
     try {
-        if (!email || !password) return res.status(400).json({ message: "Please provide email and password" });
+        if (!name || !email || !password) return res.status(400).json({ message: "Please provide email and password" });
 
         const userExist = await User.findOne({ email });
         if (userExist) return res.status(400).json({ message: "User already exists" });
 
         const hashedPassword = await bcrypt.hash(password, 12);
 
-        const result = await User.create({ email, password: hashedPassword });
+        const result = await User.create({ name, email, password: hashedPassword });
         const token = jwt.sign({ email: result.email, id: result._id }, secret, { expiresIn: "1h" });
 
         res.status(201).json({ result, token });
