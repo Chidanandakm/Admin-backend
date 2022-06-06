364b7cb21e543dd53cd65df6da1639218ec5592a
chidananda <chidananda@iterator.in>
2022-06-06 12:52:12 +0530

    category

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const { createCategory, updateCategory, getCategories, deleteCategory, getCategory } = require("../controllers/categoryController");
 const { protectedRoute } = require("../middleware/authMiddleware");
 
-router.get("/", protectedRoute, getCategories);
+router.get("/", getCategories);
 
-router.get('/:id', protectedRoute, getCategory);
+router.get('/:id', getCategory);
 
 router.post('/create', protectedRoute, createCategory);
 
